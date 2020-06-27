0e32a5dced83f4137daac9fb8235bfbbf18cf17e
Ridhanya <39475050+Ridhanya@users.noreply.github.com>
2020-06-27 21:37:31 +0530

    lightfm model

---
+++ hybrid.py
@@ -0,0 +1,148 @@
+# import dependent libraries
+import pandas as pd
+import os
+from scipy.sparse import csr_matrix
+import numpy as np
+from IPython.display import display_html
+import warnings
+
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import seaborn as sns
+import scipy.sparse as sparse
+
+from lightfm.cross_validation import random_train_test_split
+from lightfm.evaluation import auc_score, precision_at_k, recall_at_k
+from lightfm import LightFM
+#from skopt import forest_minimize
+
+def sample_recommendation_user(model, interactions, user_id, user_dict, 
+                               item_dict,threshold = 0,nrec_items = 5, show = True):
+    
+    n_users, n_items = interactions.shape
+    user_x = user_dict[user_id]
+    scores = pd.Series(model.predict(user_x,np.arange(n_items), item_features=content_interaction_csr))
+    scores.index = interactions.columns
+    scores = list(pd.Series(scores.sort_values(ascending=False).index))
+    
+    known_items = list(pd.Series(interactions.loc[user_id,:] \
+                                 [interactions.loc[user_id,:] > threshold].index).sort_values(ascending=False))
+    
+    scores = [x for x in scores if x not in known_items]
+    return_score_list = scores[0:nrec_items]
+    known_items = list(pd.Series(known_items).apply(lambda x: item_dict[x]))
+    scores = list(pd.Series(return_score_list).apply(lambda x: item_dict[x]))
+    if show == True:
+        print ("User: " + str(user_id))
+        print("Known Likes:")
+        counter = 1
+        for i in known_items:
+            print(str(counter) + '- ' + i)
+            counter+=1
+
+        print("\n Recommended Items:")
+        counter = 1
+        for i in scores:
+            print(str(counter) + '- ' + i)
+            counter+=1
+
+def recommend(person_id, sparse_person_content, person_vecs, content_vecs, num_contents=10):
+    # Get the interactions scores from the sparse person content matrix
+    person_interactions = sparse_person_content[person_id,:].toarray()
+    # Add 1 to everything, so that articles with no interaction yet become equal to 1
+    person_interactions = person_interactions.reshape(-1) + 1
+    # Make articles already interacted zero
+    person_interactions[person_interactions > 1] = 0
+    # Get dot product of person vector and all content vectors
+    rec_vector = person_vecs[person_id,:].dot(content_vecs.T).toarray()
+    
+    # Scale this recommendation vector between 0 and 1
+    min_max = MinMaxScaler()
+    rec_vector_scaled = min_max.fit_transform(rec_vector.reshape(-1,1))[:,0]
+    # Content already interacted have their recommendation multiplied by zero
+    recommend_vector = person_interactions * rec_vector_scaled
+    # Sort the indices of the content into order of best recommendations
+    content_idx = np.argsort(recommend_vector)[::-1][:num_contents]
+    
+    # Start empty list to store titles and scores
+    titles = []
+    scores = []
+
+    for idx in content_idx:
+        # Append titles and scores to the list
+        titles.append(grouped_df.title.loc[grouped_df.content_id == idx].iloc[0])
+        scores.append(recommend_vector[idx])
+
+    recommendations = pd.DataFrame({'title': titles, 'score': scores})
+
+    return recommendations
+
+articles_df = pd.read_csv('shared_articles.csv')
+interactions_df = pd.read_csv('users_interactions.csv')
+articles_df.drop(['authorUserAgent', 'authorRegion', 'authorCountry'], axis=1, inplace=True)
+interactions_df.drop(['userAgent', 'userRegion', 'userCountry'], axis=1, inplace=True)
+articles_df = articles_df[articles_df['eventType'] == 'CONTENT SHARED']
+articles_df.drop('eventType', axis=1, inplace=True)
+df = pd.merge(interactions_df[['contentId','personId', 'eventType']], articles_df[['contentId', 'title']], how = 'inner', on = 'contentId')
+#print(df['eventType'].value_counts())
+#df.to_csv('D:\\1.csv')
+event_type_strength = {
+   'VIEW': 1.0,
+   'LIKE': 2.0, 
+   'BOOKMARK': 3.0, 
+   'FOLLOW': 4.0,
+   'COMMENT CREATED': 5.0,  
+}
+df['eventStrength'] = df['eventType'].apply(lambda x: event_type_strength[x])
+df = df.drop_duplicates()
+grouped_df = df.groupby(['personId', 'contentId', 'title']).sum().reset_index()
+#print(grouped_df.head())
+grouped_df['title'] = grouped_df['title'].astype("category")
+grouped_df['personId'] = grouped_df['personId'].astype("category")
+grouped_df['contentId'] = grouped_df['contentId'].astype("category")
+grouped_df['person_id'] = grouped_df['personId'].cat.codes
+grouped_df['content_id'] = grouped_df['contentId'].cat.codes
+
+content_interaction = pd.pivot_table(grouped_df, index='person_id', columns='content_id', values='eventStrength')
+content_interaction = content_interaction.fillna(0)
+user_id = list(content_interaction.index)
+user_dict = {}
+counter = 0 
+for i in user_id:
+    user_dict[i] = counter
+    counter += 1
+content_interaction_csr = csr_matrix(content_interaction.values)
+sparse_content_person = sparse.csr_matrix((grouped_df['eventStrength'].astype(float), (grouped_df['content_id'], grouped_df['person_id'])))
+sparse_person_content = sparse.csr_matrix((grouped_df['eventStrength'].astype(float), (grouped_df['person_id'], grouped_df['content_id'])))
+
+item_dict ={}
+df = grouped_df[['content_id', 'title']].sort_values('content_id').reset_index()
+#grouped_df.to_csv("3.csv",index=False)
+#person_csr = csr_matrix(content_interaction.drop('content_id', axis=1).values)
+
+for i in range(df.shape[0]):
+    item_dict[(df.loc[i,'content_id'])] = df.loc[i,'title']
+
+#print(item_dict)
+#user_id = list(grouped_df['person_id'])
+#user_dict = {}
+#counter = 0 
+#for i in user_id:
+ #   user_dict[i] = counter
+ #   counter += 1
+#print(user_dict)
+model = LightFM(loss='warp',
+                random_state=2016,
+                learning_rate=0.90,
+                no_components=150,
+                user_alpha=0.000005)
+
+model = model.fit(content_interaction_csr,
+                  epochs=10,
+                  num_threads=16, verbose=False)
+
+#recommendations = recommend(50, sparse_person_content, user_dict, item_dict)
+#sample_recommendation_user(model, np.array((grouped_df['eventStrength'].astype(float), (grouped_df['content_id'], grouped_df['person_id']))), 50, user_dict, item_dict)
+#sample_recommendation_user(model,content_interaction_csr , 50, user_dict, item_dict)
+
+
